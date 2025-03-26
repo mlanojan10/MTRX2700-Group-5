a67de46ea46787ef7d15c30ab5f5f1a1a61197b9
Melvin  Lanojan <74356962+mlanojan10@users.noreply.github.com>
2025-03-26 20:26:47 +1100

    Change Ex1a

--- Exercise 1/Exercise1-a/Src/assembly.s
+++ Exercise 1/Exercise1-a/Src/assembly.s
@@ -1,81 +1,69 @@
 .syntax unified
-
 .thumb
 
 .global main
 
 .data
-
-input_string: .asciz "T@hiS is the INPUT string\0"
+input_string: .asciz "Hello, World!"  @ String to encode/decode
 
 .text
 
-@ Initialises the input parameters and chooses whether to convert to upper or lower.
+@ Exercise  1 c)
 main:
+    LDR R1, =input_string  @ Load address of input string into R1
+    MOV R2, #3             @ Shift value (Change for encoding/decoding)
 
-	LDR R1, =input_string @Load the address of the input string into register R1
-	LDR R2, =0x01 @This specifies whether the string will be converted to upper or lower case
-				  @(where 0 represents lower and 1 respresents upper).
-	LDR R3, =0x00 @The counter to the current place in the string.
+    BL caesar_cipher       @ Call Caesar Cipher function
 
-	CMP R2, #0x01 @Determines if we are wanting to convert to upper or lower based on the bit in register 2.
-	BEQ conversion_to_upper_loop @If the compare came back as true, the lower conversion loops are skipped.
+    B finished             @ Infinite loop (end of program)
 
-@ Traverses the string characters, and sends upper case letters to be converted to lower case.
-conversion_to_lower_loop:
+@ Function: Apply Caesar Cipher with shift value in R2
+caesar_cipher:
+    PUSH {LR}              @ Save return address
+    MOV R3, R1             @ Copy string address into R3 (iterator)
 
-	LDRB R4, [R1, R3] @This gets the current letter from the input string into the fourth register.
+cipher_loop:
+    LDRB R4, [R3]          @ Load current character
+    CMP R4, #0             @ Check if null terminator
+    BEQ cipher_done        @ If null, exit function
 
-	CMP R4, #0 @Checks for the null termination character.
-	BEQ finished_string @Skips to the finished string block.
+    CMP R4, #'A'           @ Check if char is >= 'A'
+    BLT check_lower        @ If less, check lowercase range
+    CMP R4, #'Z'           @ Check if char is <= 'Z'
+    BLE shift_upper        @ If in uppercase range, shift it
 
-	SUB R5, R4, #65 @Finds where the value would be in ASCII if a lower conversion is to occur.
-	CMP R5, 25 @Checks if the character is within the uppercase ASCII range. 25 is used because it is the range
-			   @of ASCII characters to convert with the alphabet.
-	BLS conversion_to_lower @If the compare was less than or equal to 25, the character is taken to the lower
-	 						@conversion block.
+check_lower:
+    CMP R4, #'a'           @ Check if char is >= 'a'
+    BLT next_char          @ If less, it's not a letter, skip
+    CMP R4, #'z'           @ Check if char is <= 'z'
+    BLE shift_lower        @ If in lowercase range, shift it
+    B next_char            @ Otherwise, move to next character
 
-	ADD R3, #1 @Increments the index.
+@ Shift uppercase letters
+shift_upper:
+    ADD R4, R4, R2         @ Apply shift
+    CMP R4, #'Z'           @ Check if it exceeds 'Z'
+    BLE store_char         @ If within range, store
+    SUB R4, R4, #26        @ Wrap around
+    B store_char
 
-	B conversion_to_lower_loop @Starts the process for the next character.
+@ Shift lowercase letters
+shift_lower:
+    ADD R4, R4, R2         @ Apply shift
+    CMP R4, #'z'           @ Check if it exceeds 'z'
+    BLE store_char         @ If within range, store
+    SUB R4, R4, #26        @ Wrap around
 
-@ Converts an upper case letter to lower case, and leaves lower case letters as is.
-conversion_to_lower:
+store_char:
+    STRB R4, [R3]          @ Store modified character
 
-	ADD R4, #32 @32 is the distance between upper and lower case in ASCII.
-	STRB R4, [R1, R3] @Storing the converted character (R4) into the index (R3) of the input string (R1).
+next_char:
+    ADD R3, R3, #1         @ Move to next character
+    B cipher_loop          @ Repeat loop
 
-	ADD R3, #1 @Increments the index.
-	B conversion_to_lower_loop @Starts the process for the next character.
+cipher_done:
+    POP {LR}               @ Restore return address
+    BX LR                  @ Return
 
-@ Traverses the string characters, and sends lower case letters to be converted to upper case.
-conversion_to_upper_loop:
-
-	LDRB R4, [R1, R3] @This gets the current letter from the input string into the fourth register.
-
-	CMP R4, #0 @Checks for the null termination character.
-	BEQ finished_string @Skips to the finished string block.
-
-	SUB R5, R4, #97 @Finds where the value would be in ASCII if an upper conversion is to occur.
-	CMP R5, 25 @Checks if the character is within the lowercase ASCII range. 25 is used because it is the range of
-			   @ASCII characters to convert with the alphabet.
-	BLS conversion_to_upper @If the compare was less than or equal to 25, the character is taken to the upper
-							@conversion block.
-
-	ADD R3, #1 @Increments the index.
-
-	B conversion_to_upper_loop @Starts the process for the next character.
-
-@ Converts a lower case letter to upper case, and leaves upper case letters as is.
-conversion_to_upper:
-
-	ADD R4, #-32 @32 is the distance between upper and lower case in ASCII.
-	STRB R4, [R1, R3] @Storing the converted character (R4) into the index (R3) of the input string (R1).
-
-	ADD R3, #1 @Increments the index.
-	B conversion_to_upper_loop @Starts the process for the next character.
-
-@ Infinite loop.
-finished_string:
-
-	B finished_string @Infinite loop.
+finished:
+    B finished             @ Infinite loop (end of program)
